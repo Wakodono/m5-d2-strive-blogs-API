2739cab973540f782f2691fe00b9946bf70f6888
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-10-19 22:35:14 +0100

    Update authors js file

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-/* import express from "express" // NEW IMPORT SYNTAX (remember to add type: "module" in package.json to use new syntax//)
- 
+import express from "express" // NEW IMPORT SYNTAX (remember to add type: "module" in package.json to use new syntax)
+import listEndpoints from "express-list-endpoints"
+
+import authorsRouter from "./services/authors/index.js"
+
 const server = express()
 
 server.use(express.json()) // If I do NOT specify this line BEFORE the endpoints, all the requests' bodies will be UNDEFINED
-// Remember to add the following line of code if you need to handle requests containing a json payload
 
 // ************************ ENDPOINTS **********************
 
-//server.use("/authors", authorsRouter)
+server.use("/authors", authorsRouter) // all of the endpoints which are in the authorsRouter will have /authors as a prefix
 
 const port = 3001
 
-server.listen(port, () => {
-    console.log("Server is running on port: ", port)
-}) */
+console.table(listEndpoints(server))
 
-console.log("yooooo!!!")
+server.listen(port, () => {
+  console.log("Server running on port:", port)
+})
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,15 +1,51 @@
-import express from "express" // 3RD PARTY MODULE (NEEDS to be installed)
+// 1. CREATE --> POST http://localhost:3001/authors (+ body)
+// 2. READ --> GET http://localhost:3001/authors (+ optional Query Parameters)
+// 3. READ --> GET http://localhost:3001/authors/:authorId
+// 4. UPDATE --> PUT http://localhost:3001/authors/:authorId (+ body)
+// 5. DELETE --> DELETE http://localhost:3001/authors/:authorId
+
+import express from "express" // 3RD PARTY MODULE (does need to be installed)
 import fs from "fs" // CORE MODULE (doesn't need to be installed)
-import { fileURLToPath } from "url" // CORE MODULE 
-import { dirname, join } from "path" // CORE MODULE 
-import uniqid from "uniqid" // 3RD PARTY MODULE (NEEDS to be installed)
+import { fileURLToPath } from "url" // CORE MODULE (doesn't need to be installed)
+import { dirname, join } from "path" // CORE MODULE (doesn't need to be installed)
+import uniqid from "uniqid" // 3RD PARTY MODULE (does need to be installed)
 
 const authorsRouter = express.Router() // a Router is a set of endpoints that share something like a prefix (authorsRouter is going to share /authors as a prefix)
 
 // ********************* how to find out the path *************
-// 1. Start from the current file  (src\services\authors\index.js) and get the path to that file
+// 1. I'll start from the current file and get the path to that file
 const currentFilePath = fileURLToPath(import.meta.url)
 // 2. Get the parent folder's path
 const parentFolderPath = dirname(currentFilePath)
 // 3. Concatenate the parent's folder path with authors.json
 const authorsJSONPath = join(parentFolderPath, "authors.json") // DO NOT EVER USE '+' TO CONCATENATE TWO PATHS, USE JOIN INSTEAD
+
+// 1.
+authorsRouter.post("/", (req, res) => {
+  // First parameter is relative URL, second parameter is the REQUEST HANDLER
+
+  // 1. Read the request body obtaining the new author's data
+  console.log(req.body)
+
+  const newAuthor = { 
+      ...req.body, 
+      id: uniqid() 
+    }
+  console.log(newAuthor)
+
+  // 2. Read the file content obtaining the authors array
+  const authors = JSON.parse(fs.readFileSync(authorsJSONPath))
+
+  // 3. Add new author to the array
+  authors.push(newAuthor)
+
+  // 4. Write the array back to the file
+  fs.writeFileSync(authorsJSONPath, JSON.stringify(authors))
+
+  // 5. Send back a proper response
+
+  res.status(201).send({ id: newAuthor.id })
+})
+
+
+export default authorsRouter
